57a53f1f753ed6cf0cd1d2676343d5eb9c7a64a2
Drax <bader.laszlo22@gmail.com>
2015-08-18 17:03:38 +0200

    nono

--- page/includes/header.php
+++ page/includes/header.php
@@ -4,15 +4,13 @@
         <meta charset="utf-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-
         <link href="data:image/x-icon;base64,AAABAAEAEBAQAAEABAAoAQAAFgAAACgAAAAQAAAAIAAAAAEABAAAAAAAgAAAAAAAAAAAAAAAEAAAAAAAAAD/hAAA////AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAEQAAARAAABEQAAABEAAAAQAAEREREQAAAAEREREREAAAARABEAEQAAABEAEQARAAAAEQAAABEAAAARAAAAEQAAABEAAAARAAAAEQAAABEAAAARABEAEQAAABEAEQARAAAAEREREREAAAABEREREAABAAAAEQAAABEQAAARAAABEAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA" rel="icon" type="image/x-icon" />
         <meta name="description" content="Báder László Weboldal Önéletrajz" />
         <meta name="keywords" content="Báder László Weboldal Önéletrajz Materialize Bootstrap" />
         <meta name="author" content="metatags generator">
         <meta name="robots" content="index, follow">
         <meta name="revisit-after" content="1 month">
-        <title>Báder László - Weboldal Önéletrajz</title>
-
+        <title>Báder László - CV</title>
 
         <!-- Materialize -->              
         <link href="page/css/materialize.css" rel="stylesheet">
@@ -30,7 +28,7 @@
         <link href="page/css/main.css" rel="stylesheet">
 
         <!-- Custom CSS -->        
-        <link href="page/css/animate.css" rel="stylesheet">
+        <link href="page/css/animate.min.css" rel="stylesheet">
         <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css">
 
@@ -38,7 +36,7 @@
         <link href="page/css/media.css" rel="stylesheet">
 
         <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->        
-        <!--[if lt IE 9]>
+        <!--[if gte IE 9]>
           <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
           <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
         <![endif]-->
